85bbf24356be5999a413083536507c4293433c8e
imarshbir <singharshbir76@gmail.com>
2026-02-21 19:35:06 +0530

    updates 1

---
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+
+export default function DashboardLayout() {
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+
+      <Sidebar />
+
+      <div className="flex-1 p-6">
+        <Outlet />
+      </div>
+
+    </div>
+  );
+}
